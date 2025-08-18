7d6816d6f766040de37a30ef850654aaf783e8e6
FerG0373 <fer.dg.sc@gmail.com>
2025-08-18 09:25:58 -0300

    Preparando entorno

---
+++ src/main/java/com/aluracursos/forohub_challenge/ForohubChallengeApplication.java
@@ -0,0 +1,18 @@
+package com.aluracursos.forohub_challenge;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForohubChallengeApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForohubChallengeApplication.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hola MUNDO");
+    }
+}
---
+++ src/test/java/com/aluracursos/forohub_challenge/ForohubChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.forohub_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForohubChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
